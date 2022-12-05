437cf0c6bbb1640bc7fdd56234e9b99aa60b6b09
CristopherMartins <cmartins@bonadmin.com>
2022-12-05 11:29:09 -0300

    any name load the file

---
+++ anything.vue
@@ -0,0 +1,19 @@
+<template>
+    <nav>
+        <a href="#" @click="view('home')">Home</a>
+        <a href="#" @click="view('counter')">Counter</a>
+    </nav>
+
+    <NuxtPage />
+    
+    <LazyHomeWelcome v-if="page == 'home'" />
+    <LazyCounter v-if="page == 'counter'" />
+</template>
+
+<script setup>
+    const page = ref("home");
+
+    function view(name){
+        page.value = name;
+    }
+</script>
