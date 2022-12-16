62852c7ef499c888a5e7a1ffc01e9dd553fb0002
CristopherMartins <cmartins@bonadmin.com>
2022-12-16 15:16:56 -0300

    change hello name

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
 
     <h1>Hello World</h1>
 
-    <h1>{{ $t('hello', { name: 'vue-i18n' }) }}</h1>
+    <h1>{{ $t('hello', { name: 'Cris' }) }}</h1>
 
     <button @click="about">About page</button>
 </template>
